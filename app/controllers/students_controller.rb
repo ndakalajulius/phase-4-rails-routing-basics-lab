e1edb94b69ad32d46681eb6d48486d9e9fc18e87
ndakalajulius <julius.ndakala@student.moringaschool>
2022-11-21 21:08:30 +0300

    done

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -2,6 +2,14 @@ class StudentsController < ApplicationController
     def index
         students = Student.all
         render json: students
-      end
+    end
+   
+    def grades
+        s =  Student.order("grade").reverse
+        render json: s
+    end
 
+    def highest_grade
+        render json: Student.order("grade").reverse.first
+    end
 end
